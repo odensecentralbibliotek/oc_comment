23b051fabb90afca1067f71d858c13540c9e7511
Tobias F. Petersen <tobsf@hotmail.com>
2015-08-27 14:29:10 +0200

    added logic to insert queries based on sort direction.

--- js/oc_comments.js
+++ js/oc_comments.js
@@ -63,14 +63,32 @@ function bind_form_submit()
                     if(Drupal.settings.oc_comment.skip_approval)
                     {
                         jQuery(".submit-form-error-message").append('<div class="messages status">Comment Postet</div>');
+                        var new_elem = jQuery(msg.markup);
+                        new_elem.hide();
                         if(Drupal.settings.oc_comment.top_level_comment_sort == "DESC")
                         {
-                            jQuery('#oc-comments-wrap').prepend(msg.markup);
+                            jQuery('#oc-comments-wrap').prepend(new_elem);
                         }
                         else
                         {
-                            jQuery('#oc-comments-wrap').append(msg.markup);
+                            var page_count = jQuery('.pager li').length;
+                            var goto_first_btn_exits = jQuery('.pager-first').length;
+                            if(!goto_first_btn_exits &&  page_count<= 1)
+                            {
+                               jQuery('#oc-comments-wrap').append(new_elem);
+                            }
+                            //If we are on page 0 insert , else dont.
+                            
                         }
+                        new_elem.fadeIn("slow");
+                        new_elem.pulsate({
+                            reach: 20, // how far the pulse goes in px
+                            speed: 1000, // how long one pulse takes in ms
+                            pause: 0, // how long the pause between pulses is in ms
+                            glow: true, // if the glow should be shown too
+                            repeat: 1, // will repeat forever if true, if given a number will repeat for that many times
+                            onHover: false                          // if true only pulsate if user hovers over the element
+                        });
                     }
                     else
                     {
